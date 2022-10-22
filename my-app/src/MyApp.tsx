20874ad560bf494adb8bed27fd3fd00a648b7db2
odikyudi69@gmail.com <O-dik90>
2022-10-22 11:40:51 +0700

    update custom hooks useFertch

--- my-app/src/MyApp.tsx
+++ my-app/src/MyApp.tsx
@@ -1,4 +1,12 @@
-import { ReactNode, useContext, useEffect, useRef, useState } from "react";
+import {
+  ReactNode,
+  // useCallback,
+  useContext,
+  useMemo,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import {
   Button,
   ButtonGroup,
@@ -10,10 +18,12 @@ import {
   Row,
 } from "react-bootstrap";
 import { UserContext } from ".";
+import useFetch from "./useFetch";
 
 interface MyAppProps {
   title?: string;
-  children?: ReactNode;
+  children?:ReactNode;
+  
 }
 
 export function MyApp({ title }: MyAppProps) {
@@ -34,13 +44,44 @@ export function MyApp({ title }: MyAppProps) {
   // useRef
   // belum full understanding, maksud dari mereferensikannya
   const Input = useRef(null);
-
   const Click = () => {
     console.log(Input);
     console.log(Input.current);
   };
+
+  // usememo
+  function squareNum(numb: number) {
+    console.log("Squaring will be done!");
+    return Math.pow(numb, 2);
+  }
+  const [numb, setNumb] = useState(0);
+  // tanpa usememo
+  // const square = squareNum(numb);
+  // dengan usememo
+  const square = useMemo(() => {
+    return squareNum(numb);
+  }, [numb]);
+  //set counter
+  const [counter, setCounter] = useState(0);
+  // Change the state to the input
+  const onChangeHandler = (e: any) => {
+    setNumb(e.target.value);
+  };
+
+  // Increases the counter by 1
+  const counterHander = () => {
+    setCounter(counter + 1);
+  };
+
+  //custom Hooks
+  //create custom hooks from other
+  //masalahnya adalah:
+  // akan terpanggil ketikaa rendering-ulang
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos/")
+
   return (
     <>
+    {console.log(data)}
       <p className="fs-4">{title}</p>
       <p>{state}</p>
       <ButtonGroup>
@@ -55,7 +96,7 @@ export function MyApp({ title }: MyAppProps) {
           Diff State
         </Button>
       </ButtonGroup>
-      <Row className="justify-content-center mt-3">
+      <Row className="justify-content-center my-3">
         <Col sm={5}>
           <InputGroup className="mb-3">
             <Form.Control
@@ -87,6 +128,24 @@ export function MyApp({ title }: MyAppProps) {
           </Col>
         </Row>
       </Container>
+
+      <Row className="justify-content-center">
+        <Col xs={4}>
+          <InputGroup className="my-3">
+            <Form.Control
+              placeholder="Entry"
+              type="number"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              value={numb}
+              onChange={onChangeHandler}
+            />
+            <Button onClick={counterHander}>Counter ++</Button>
+          </InputGroup>
+        </Col>
+        <p>Output: {square}</p>
+        <p>Counter: {counter}</p>
+      </Row>
     </>
   );
-};
+}
